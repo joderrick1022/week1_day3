17cba5ba50b13fd50f49e579b69d328e1d978e7c
joderrick1022 <joderrick1022>
2017-09-27 13:09:14 -0400

    started with if/else!

--- week1_day3/Program.cs
+++ week1_day3/Program.cs
@@ -10,6 +10,41 @@ namespace week1_day3
     {
         static void Main(string[] args)
         {
+            /*         int firstNumber = 2;
+                     int secondNumber = 6;
+
+
+                     if (firstNumber < secondNumber)
+
+                     {
+                         // do something
+                         concole.WriteLine("you have broken math. Good job!");
+
+                     }
+                     else
+                     {
+                         Console.WriteLine("That makes sense!");
+                  */
+
+
+            Console.WriteLine("what day is today");
+            string userAnswer = Console.ReadLine();
+
+            if (userAnswer == "monday.")
+            {
+                Console.WriteLine("Back to work");
+            }
+            else if (userAnswer == "Friday")
+
+
+                    {
+                Console.WriteLine("TGIF");
+            }
+            
+
+
+
         }
+        
     }
 }
